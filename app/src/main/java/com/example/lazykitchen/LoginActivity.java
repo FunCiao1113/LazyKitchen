9f8651234a9e9a299297f760d8682e6ca25974a6
Boomhao1324 <2896263107@qq.com>
2021-05-14 11:30:58 +0800

    create

---
+++ app/src/main/java/com/example/lazykitchen/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.lazykitchen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button getCode= findViewById(R.id.getCode);
+        getCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,VerifyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lazykitchen/VerifyActivity.java
@@ -0,0 +1,25 @@
+package com.example.lazykitchen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VerifyActivity.this,VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
