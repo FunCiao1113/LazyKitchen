1d76edbb59ac8ead94aa4fc4e50248f6cb23af47
Boomhao1324 <2896263107@qq.com>
2021-06-14 15:50:28 +0800

    视频播放界面

---
+++ app/src/main/java/com/example/lazykitchen/util/OnRecyclerItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.lazykitchen.util;
+
+import java.util.List;
+
+public interface OnRecyclerItemClickListener {
+    void onItemClick(int Position, List<VideoItem> videoItemList);
+}
