ca8f9dd047e4586089d3dfd598569bb826100e89
Alleyisland <736386360@qq.com>
2021-06-16 14:47:27 +0800

    user info load & update

--- app/src/main/java/com/example/lazykitchen/activity/HomeActivity.java
+++ app/src/main/java/com/example/lazykitchen/activity/HomeActivity.java
@@ -5,20 +5,43 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceManager;
 
 import android.app.ActionBar;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.lazykitchen.R;
 import com.example.lazykitchen.util.ActivityUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String userDataUrlPrefix="http://47.100.4.109:8080/user/info";
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        intent=getIntent();
+        syncLocalUserData();
         this.getSupportActionBar().hide();
         ActivityUtils.add(this.getClass().getSimpleName(),this);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -28,5 +51,60 @@ public class HomeActivity extends AppCompatActivity {
         AppBarConfiguration configuration = new AppBarConfiguration.Builder(R.id.searchFragment,R.id.shareFragment,R.id.personFragment).build();
         NavigationUI.setupActionBarWithNavController(this,navController,configuration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    public void syncLocalUserData(){
+        OkHttpClient client = new OkHttpClient();
+        //String phone=intent.getStringExtra("phone");
+        String phone="17321050252";
+        String userDataUrl=userDataUrlPrefix+"?phone="+phone;
+        Request request = new Request.Builder()
+                .url(userDataUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Toast toast = Toast.makeText(HomeActivity.this, "获取用户信息失败", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    Gson gson=new Gson();
+                    Map map=gson.fromJson(response.body().string(),Map.class);
+                    // code=0->成功 code=-1->失败
+                    String name=((Map)map.get("userInfo")).get("name").toString();
+                    String sex=((Map)map.get("userInfo")).get("gender").toString();
+                    //处理UI需要切换到UI线程处理
+                    runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            // 更新逻辑写在这里
+                            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(HomeActivity.this);
+                            prefs.edit().putString("name",name).apply();
+                            String sexCode="2";
+                            if(sex.equals("男")) {
+                                sexCode = "0";
+                            }
+                            else if (sex.equals("女")){
+                                sexCode= "1";
+                            }
+                            prefs.edit().putString("sex",sexCode).apply();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lazykitchen/fragment/SettingFragment.java
+++ app/src/main/java/com/example/lazykitchen/fragment/SettingFragment.java
@@ -1,18 +1,129 @@
 package com.example.lazykitchen.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.example.lazykitchen.R;
+import com.example.lazykitchen.activity.HomeActivity;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
-public class SettingFragment extends PreferenceFragmentCompat {
+public class SettingFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
 
+    private EditTextPreference namePreference = null;
+    private ListPreference sexPreference = null;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    String updateInfoUrl="http://47.100.4.109:8080/user/update_info";
+    String[] sex=new String[]{"男","女","未知"};
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        namePreference = findPreference("name");
+        sexPreference = findPreference("sex");
+        namePreference.setOnPreferenceChangeListener(this);
+        sexPreference.setOnPreferenceChangeListener(this);
+
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(getActivity());
+        namePreference.setSummary(prefs.getString("name","厨房新人"));
+        sexPreference.setSummary(sex[Integer.parseInt(prefs.getString("sex","2"))]);
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String newName = null;
+        String newSex = null;
+        if(preference==namePreference){
+            newName=(String) newValue;
+            //namePreference.setSummary(newName);
+        }
+        else if(preference==sexPreference){
+            newSex=sex[Integer.parseInt((String) newValue)];
+            //sexPreference.setSummary(newSex);
+        }
+        updateUserInfo(newName,newSex);
+        return true;
+    }
+
+    public void updateUserInfo(String name,String sex){
+        OkHttpClient client = new OkHttpClient();
+        long id=1;
+        JsonObject json=new JsonObject();
+        json.addProperty("id",id);
+        if(name!=null) {
+            json.addProperty("name", name);
+        }
+        else if(sex!=null) {
+            json.addProperty("gender", sex);
+        }
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        System.out.println(json.toString());
+        Request request = new Request.Builder()
+                .url(updateInfoUrl)
+                .post(body)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //...
+                getActivity().runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Toast toast = Toast.makeText(getActivity(), "修改用户信息失败", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    Gson gson=new Gson();
+                    Map map=gson.fromJson(response.body().string(),Map.class);
+                    // code=0->成功 code=-1->失败
+                    System.out.println(map.get("code").toString());
+                    //处理UI需要切换到UI线程处理
+                    getActivity().runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            // 更新逻辑写在这里
+                            Toast toast = Toast.makeText(getActivity(), "修改用户信息成功", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
